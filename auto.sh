c1a334debb120236354b0273bdfa22eb8d13327b
Mehmet Sinc <55138973+mehmetsinc@users.noreply.github.com>
2023-07-17 12:53:57 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Sunucu Hostname'i
+hostname=$(hostname)
+
 # Master Sunucu Ayarları
 master_ip="10.16.0.230"
 master_hostname="16230-master"
@@ -8,44 +11,57 @@ master_hostname="16230-master"
 slave_ip="10.16.0.229"
 slave_hostname="16229-slave"
 
-# Master Sunucu Yapılandırması
-echo "[1/6] Master Sunucu Yapılandırılıyor..."
-echo "server-id=1" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "log_bin = /var/log/mysql/mysql-bin.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "binlog_format = ROW" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "expire_logs_days = 10" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "max_binlog_size = 100M" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "bind-address = 0.0.0.0" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "log_error = /var/log/mysql/error.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+if [[ $hostname == $master_hostname ]]; then
+    # Master Sunucu Yapılandırması
+    echo "[1/6] Master Sunucu Yapılandırılıyor..."
+    echo "server-id=1" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "log_bin = /var/log/mysql/mysql-bin.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "binlog_format = ROW" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "expire_logs_days = 10" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "max_binlog_size = 100M" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "bind-address = 0.0.0.0" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "log_error = /var/log/mysql/error.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# Slave Sunucu Yapılandırması
-echo "[2/6] Slave Sunucu Yapılandırılıyor..."
-echo "server-id=2" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "log_bin = /var/log/mysql/mysql-bin.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "binlog_format = ROW" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "expire_logs_days = 10" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "max_binlog_size = 100M" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "bind-address = 0.0.0.0" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "log_error = /var/log/mysql/error.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    # Mariadb Servisleri Yeniden Başlatılıyor
+    echo "[2/6] Mariadb Servisleri Yeniden Başlatılıyor..."
+    systemctl restart mysql
 
-# Mariadb Servisleri Yeniden Başlatılıyor
-echo "[3/6] Mariadb Servisleri Yeniden Başlatılıyor..."
-systemctl restart mysql
+    # Master Sunucuda Replication Kullanıcısı Oluşturuluyor
+    echo "[3/6] Master Sunucuda Replication Kullanıcısı Oluşturuluyor..."
+    mysql -u root -e "CREATE USER 'repl_user'@'%' IDENTIFIED BY 'repl_password';"
+    mysql -u root -e "GRANT REPLICATION SLAVE ON *.* TO 'repl_user'@'%';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
 
-# Master Sunucuda Replication Kullanıcısı Oluşturuluyor
-echo "[4/6] Master Sunucuda Replication Kullanıcısı Oluşturuluyor..."
-mysql -u root -e "CREATE USER 'repl_user'@'%' IDENTIFIED BY 'repl_password';"
-mysql -u root -e "GRANT REPLICATION SLAVE ON *.* TO 'repl_user'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+    # Master Sunucuda Binary Log Aktifleştiriliyor
+    echo "[4/6] Master Sunucuda Binary Log Aktifleştiriliyor..."
+    mysql -u root -e "PURGE BINARY LOGS BEFORE NOW();"
+    mysql -u root -e "RESET MASTER;"
 
-# Slave Sunucuda Replication Ayarları Yapılıyor
-echo "[5/6] Slave Sunucuda Replication Ayarları Yapılıyor..."
-mysql -u root -e "CHANGE MASTER TO MASTER_HOST='$master_hostname', MASTER_USER='repl_user', MASTER_PASSWORD='repl_password', MASTER_AUTO_POSITION=1;"
-mysql -u root -e "START SLAVE;"
+    echo "Master Sunucu yapılandırması tamamlandı."
 
-# Master Sunucuda Binary Log Aktifleştiriliyor
-echo "[6/6] Master Sunucuda Binary Log Aktifleştiriliyor..."
-mysql -u root -e "PURGE BINARY LOGS BEFORE NOW();"
-mysql -u root -e "RESET MASTER;"
+elif [[ $hostname == $slave_hostname ]]; then
+    # Slave Sunucu Yapılandırması
+    echo "[1/4] Slave Sunucu Yapılandırılıyor..."
+    echo "server-id=2" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "log_bin = /var/log/mysql/mysql-bin.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "binlog_format = ROW" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "expire_logs_days = 10" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "max_binlog_size = 100M" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "bind-address = 0.0.0.0" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+    echo "log_error = /var/log/mysql/error.log" >> /etc/mysql/mariadb.conf.d/50-server.cnf
 
-echo "Yapılandırma tamamlandı."
+    # Mariadb Servisleri Yeniden Başlatılıyor
+    echo "[2/4] Mariadb Servisleri Yeniden Başlatılıyor..."
+    systemctl restart mysql
+
+    # Slave Sunucuda Replication Ayarları Yapılıyor
+    echo "[3/4] Slave Sunucuda Replication Ayarları Yapılıyor..."
+    mysql -u root -e "CHANGE MASTER TO MASTER_HOST='$master_ip', MASTER_USER='repl_user', MASTER_PASSWORD='repl_password', MASTER_AUTO_POSITION=1;"
+    mysql -u root -e "START SLAVE;"
+
+    echo "Slave Sunucu yapılandırması tamamlandı."
+
+else
+    echo "Bilinmeyen bir sunucu hostname'i algılandı."
+    exit 1
+fi
